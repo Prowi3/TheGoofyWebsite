f9d854bced0ed1c9e9331676d2c39cda0c83deda
Aleph <142549725+Prowi3@users.noreply.github.com>
2023-09-30 14:20:58 +0000

    finally learning how to read and remember

--- app.js
+++ app.js
@@ -16,17 +16,15 @@ hiddenElements.forEach((el) => observer.observe(el));
 const burgerMenu = document.querySelector('.burger-menu');
 const sideMenu = document.getElementById('sideMenu');
 
-// Function to toggle the side menu
 function toggleSideMenu() {
     sideMenu.classList.toggle('active');
 }
 
 burgerMenu.addEventListener('click', function (event) {
-    event.stopPropagation(); // Prevent the click from reaching the document
+    event.stopPropagation();
     toggleSideMenu();
 });
 
-// Close the side menu when a menu item is clicked
 const menuLinks = document.querySelectorAll('.side-menu a');
 menuLinks.forEach(function (item) {
     item.addEventListener('click', function () {
@@ -34,14 +32,12 @@ menuLinks.forEach(function (item) {
     });
 });
 
-// Close the side menu when clicking outside of it
 document.addEventListener('click', function (event) {
     if (!sideMenu.contains(event.target) && !burgerMenu.contains(event.target)) {
         sideMenu.classList.remove('active');
     }
 });
 
-// Close the side menu when scrolling the page
 window.addEventListener('scroll', function () {
     if (sideMenu.classList.contains('active')) {
         sideMenu.classList.remove('active');
